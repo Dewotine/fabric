763f846a66f80583295c5cd7099caea6d29d8bc8
Cedric <cedric.bleschet@gmail.com>
2015-02-15 14:29:42 +0100

    Premier commit

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*-coding:utf-8 -*
+################################################
+#    FABRIC FILE PRODUCTION ENVIRONMENT
+##################################################
+
+def hello():
+    print("Hello World!")
+
+
+
+#@task
+#def install(pkg=None):
+#   if pkg is not None:
+#      env["pkg"] = pkg
+#   elif pkg is None and env.get("pkg") is None:
+#      env["pkg"] = prompt("Which package? ")
+#   sudo('yum install -y %s' % env["pkg"])
